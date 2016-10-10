40193c9ed0a7c8c7c7b0338a19bb326a4be976a8
Gabriel Lett <glett@bionexo.com>
2016-10-10 08:05:32 -0300

    Add tests and initial codew

---
+++ src/index.js
@@ -0,0 +1,36 @@
+import fetch from 'isomorphic-fetch';
+
+const checkStatus = (response) => {
+  const error = new Error(response.statusText);
+
+  if (response.status >= 200 && response.status < 300) {
+    return response;
+  }
+
+  error.response = response;
+  throw error;
+};
+
+
+export default (getUrl, setUrl, options) => ({
+  load() {
+    return fetch(getUrl, options)
+      .then(checkStatus)
+      .then(response => response.json())
+      .catch(error => Promise.reject(error.message));
+  },
+
+  save(store) {
+    const saveOptions = {
+      ...options,
+      method: 'POST',
+      body: JSON.stringify(store),
+      headers: {
+        Accept: 'application/json',
+        'Content-Type': 'application/json',
+      },
+    };
+
+    return fetch(setUrl, saveOptions).then(checkStatus);
+  },
+});
---
+++ test/index.test.js
@@ -0,0 +1,65 @@
+import chai from 'chai';
+import chaiAsPromised from 'chai-as-promised';
+import nock from 'nock';
+import createEngine from '../src/';
+
+chai.use(chaiAsPromised);
+const expect = chai.expect;
+
+describe('engine', () => {
+  describe('#load', () => {
+    context('when the request go well', () => {
+      it('should fetch store from remote', () => {
+        nock('http://test.localhost')
+          .get('/some/url.json')
+          .reply(200, { test: 'store' });
+
+        const engine = createEngine('http://test.localhost/some/url.json', 'whatever');
+        return expect(engine.load()).to.eventually.eql({ test: 'store' });
+      });
+    });
+
+    context('when the request returns an error', () => {
+      it('should reject if the remote returns an error code', () => {
+        nock('http://test.localhost')
+          .get('/some/url.json')
+          .reply(500);
+
+        const engine = createEngine('http://test.localhost/some/url.json', 'whatever');
+        return expect(engine.load()).to.eventually.be.rejectedWith('Internal Server Error');
+      });
+    });
+  });
+
+  describe('#save', () => {
+    context('when the request go well', () => {
+      it('should save sending a post to the post url with the store', () => {
+        nock('http://test.localhost', {
+          reqheaders: {
+            Accept: 'application/json',
+            'Content-Type': 'application/json',
+          },
+        }).post('/some/other/url', JSON.stringify({ test: 'store' }))
+        .reply(200, { some: 'potato' });
+
+        const engine = createEngine('http://test.localhost:80/some/other/url', 'http://test.localhost:80/some/other/url');
+        return expect(engine.save({ test: 'store' })).to.eventually.have.property('status').eq(200);
+      });
+    });
+
+    context('when the request returns an error', () => {
+      it('should reject if the remote returns an error code', () => {
+        nock('http://test.localhost', {
+          reqheaders: {
+            Accept: 'application/json',
+            'Content-Type': 'application/json',
+          },
+        }).post('/some/other/url', JSON.stringify({ test: 'store' }))
+        .reply(500);
+
+        const engine = createEngine('http://test.localhost:80/some/other/url', 'http://test.localhost:80/some/other/url');
+        return expect(engine.save({ test: 'store' })).to.eventually.be.rejectedWith('Internal Server Error');
+      });
+    });
+  });
+});
